fe7522aa8b896d262c6ddee64e94d736f960b2b6
Fernando Silva Noleto <fernandonoleto17@gmail.com>
2020-05-03 12:19:16 -0300

    Iniciando projetos com Web Services Api com Asp Net Core.

---
+++ api-web-asp-net-core/Controllers/ValuesController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace api_web_asp_net_core.Controllers
+{
+    [Route("api/webservice")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        // GET api/values
+        [HttpGet]
+        public ActionResult<IEnumerable<string>> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/webservice/buscarcliente/5
+        [HttpGet("{id}")]
+        [Route("buscarcliente/{id}")]
+        public ActionResult<string> BuscarCliente(int id)
+        {
+            return id.ToString();
+        }
+
+        // POST api/values
+        [HttpPost]
+        [Route("cadastrarcliente")]
+        public string CadastrarCliente([FromBody] string value)
+        {
+            return "oi, fernando";
+        }
+
+        // PUT api/values/5
+        [HttpPut("{id}")]
+        [Route("putcliente/{id}")]
+        public void PutCliente(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
