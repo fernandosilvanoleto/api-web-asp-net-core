7c5a970047ceab3998b287f13004165f419f8f75
Fernando Silva Noleto <fernandonoleto17@gmail.com>
2020-05-04 18:12:59 -0300

    Consumindo API para métodos GET e POST.

---
+++ ConsumindoApi/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Text;
+
+namespace ConsumindoApi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //var request = (HttpWebRequest)WebRequest.Create("https://localhost:44374/api/webservice/BuscarCliente/987");
+            //var response = (HttpWebResponse)request.GetResponse();
+            // var responseString = new System.IO.StreamReader(response.GetResponseStream()).ReadToEnd();
+
+            //Console.WriteLine("A resposta do método GET é: " + responseString);
+
+            var request = (HttpWebRequest)WebRequest.Create("https://localhost:44374/api/webservice/cadastrarcliente");
+            var posData = "'variavel1':'valor'";
+            var data = Encoding.ASCII.GetBytes(posData);
+            request.Method = "POST";
+            request.ContentType = "application/json";
+            request.ContentLength = data.Length;
+
+            using(var strem = request.GetRequestStream())
+            {
+                strem.Write(data, 0, data.Length);
+            }
+            var response = (HttpWebResponse) request.GetResponse();
+            var responseString = new StreamReader(response.GetResponseStream()).ReadToEnd();
+
+            Console.WriteLine("A resposta do método POST é: " + responseString);
+
+        }
+    }
+}
